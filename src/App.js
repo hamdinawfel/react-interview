1757da82e6e89261e5d38f84180822577005c899
HamdiNawfel <hamdii.nawfel@gmail.com>
2022-02-01 23:12:14 +0100

    build : test tabs application ui with taolwind

--- src/App.js
+++ src/App.js
@@ -4,14 +4,17 @@ import Services from "./components/Services";
 import Work from "./components/Work";
 import CallToAction from "./components/CallToAction";
 import AspectRatio from "./tailwindLabs/AspectRatio";
+import Tabs from "./components/Tabs";
 
 import Footer from "./components/Footer";
 import Card from "./components/Card";
+// import Tabs from "./components/Tabs";
 
 function App() {
   return (
     <div>
       <Nav />
+      <Tabs />
       {/*  <Hero />
       <Services />
       <Work />
